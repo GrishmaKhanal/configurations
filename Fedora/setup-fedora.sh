1bea2ba42b984023acb899b83fb71258352dcc0e
GrishmaKhanal <grishmakhanal48@gmail.com>
2025-02-28 22:42:50 +0545

    Enhance C++ build configuration and update Flatpak applications list; streamline Fedora setup script

--- Fedora/setup-fedora.sh
+++ Fedora/setup-fedora.sh
@@ -39,24 +39,20 @@ post_reboot_tasks() {
 
     # Install software
     echo "Installing software..."
-    sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda steam fastfetch gnome-tweaks zsh gh expect lutris
+    # sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda steam fastfetch gnome-tweaks zsh gh expect lutris
+    sudo dnf install -y fastfetch gnome-tweaks zsh gh expect
 
     # Install Flatpak applications
     echo "Installing Flatpak applications..."
     xargs flatpak install -y < flatpak-files.txt
     sudo dnf remove firefox -y
 
-    # Clone GitHub repository and run setup script
-    cd ~/Workspace/Different-Config/Fedora
-    cp begin.sh ~/
-    cp game.sh ~/
-
-    echo "Loading Custom Keybindings - Gnome"
-    cd Keybindings/
-    dconf load /org/gnome/shell/keybindings/ < bkp_shell
-    dconf load /org/gnome/desktop/wm/keybindings/ < bkp_wm
-    dconf load /org/gnome/settings-daemon/plugins/media-keys/ < bkp_media
-    cd ~
+    # echo "Loading Custom Keybindings - Gnome"
+    # cd Keybindings/
+    # dconf load /org/gnome/shell/keybindings/ < bkp_shell
+    # dconf load /org/gnome/desktop/wm/keybindings/ < bkp_wm
+    # dconf load /org/gnome/settings-daemon/plugins/media-keys/ < bkp_media
+    # cd ~
 
     # Final message & getting oh my zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
